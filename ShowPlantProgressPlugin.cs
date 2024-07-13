72f060948c7a2c496b3e6ee8537c1ef6bf15cbc9
KompjoeFriek <roel@kompjoefriek.nl>
2024-07-13 16:00:30 +0200

    Basic start of a Valheim plugin
    VS2022 .NET desktop environment with .NET Framework 4.6.2
    Project generated with bepinex5plugin template: https://docs.bepinex.dev/articles/dev_guide/plugin_tutorial/index.html
    Just changed target from net46 to net462 (fixes missing reference to netstandard)

---
+++ ShowPlantProgressPlugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace ShowPlantProgress;
+
+[BepInPlugin(_modUid, _modDescription, _modVersion)]
+public class ShowPlantProgressPlugin : BaseUnityPlugin
+{
+    internal const string _modVersion = "1.5.0";
+    internal const string _modDescription = "Show Plant Progress";
+    internal const string _modUid = "kompjoefriek.showplantprogress";
+
+    internal static new ManualLogSource Logger;
+        
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {_modUid} is loaded!");
+    }
+}
